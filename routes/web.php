82b0be0065dce3ce18813a39768d29eee23c7636
joyun <joyunji1119@gmail.com>
2024-03-21 17:07:58 +0900

    完了-編集機能

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,8 @@ Route::get('/', function () {
 });
 
 Route::get('/list', [TodoListController::class, 'index']);
+
+Route::resource('tasks', TaskController::class);
+// Route::post('/create', [TaskController::class, 'create']);
+// Route::post('/update', [TaskController::class, 'update']);
+// Route::post('/delete', [TaskController::class, 'delete']);
